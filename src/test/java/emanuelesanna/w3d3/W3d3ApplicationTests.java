9eb5eef4214735a512586f8184e054b06fd83570
EmanSan9 <sanna950@gmail.com>
2025-10-22 16:54:29 +0200

    Initial commit

---
+++ src/test/java/emanuelesanna/w3d3/W3d3ApplicationTests.java
@@ -0,0 +1,13 @@
+package emanuelesanna.w3d3;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class W3d3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
